f1f0d590f276b9356172713dfcfb6b951bb0cf0f
Sajal-Aggarwal <82278861+Sajal-Aggarwal@users.noreply.github.com>
2022-01-30 17:01:34 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,41 +1,29 @@
-//Express is a minimal and flexible Node.js web application framework that provides a 
-//robust set of features for web and mobile applications. 
-//It helps in fast-tracking development of server-based applications.
+
 const express=require('express');
 
-// Path is an inbuilt module in npm and need not be installed
 const path=require('path');
 
-// The port no. on which we see in the browser. In this case, it is localhost:8000
 const port=8000;
 
-// The bodyParser object exposes various factories to create middlewares as variable
 var bodyParser = require('body-parser');
 
-// require mongoose
 const db= require('./config/mongoose');
 
-// accesing the task through router
 const Task=require('./models/task');
 
 const app= express();
 
-// Setting ejs as the view engine for the web app
 app.set('view engine' ,'ejs');
 
-/*The use of __dirname helps generalise the path so that it need not be altered by the user 
  when viewing the file on his system*/
 app.set('views',path.join(__dirname,'views'));
 
-// Middleware which encodes the url
 app.use(express.urlencoded());
 
-// Accessing the static files
 app.use(express.static('assets'));
 
 app.use(express.urlencoded({ extended: true }));
 
-// Get the task through routing with get method (make a request and get response) and use controller for action
 app.get('/', function(req,res){
     Task.find({}, function(err,tasks){
         if(err){
@@ -86,7 +74,6 @@ app.post('/delete-task', function(req, res){
 
 app.listen(port,function(err){
     if(err){
-        // using interpolation
         console.log(`Error in running the server: ${err}`);
         return;
     }
